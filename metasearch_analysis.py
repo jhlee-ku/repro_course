f63898ddb7c64ea7bd3289c0269849ed1c46d639
Jong-Hwan Lee <jhlee.jonghwanlee@gmail.com>
2017-03-15 10:36:07 -0400

    add a comment on data download

--- metasearch_analysis.py
+++ metasearch_analysis.py
@@ -1,5 +1,2 @@
 # coding: utf-8
-#get_ipython().system('git clone https://github.com/OpenNeuroLab/metasearch.git')
-#get_ipython().magic('history -n -g')
-roi_volumes = [2.73, 145.3, 12.7, 16.2, 'a', 'b']
-get_ipython().magic('history -g')
+#Download our data from openneu.ro
