0ad9970bfc2fbb522292aa263bd928a9cdd4ee48
Jong-Hwan Lee <jhlee.jonghwanlee@gmail.com>
2017-03-15 11:28:37 -0400

    move metasearch into untracked directory

--- metasearch_analysis.py
+++ metasearch_analysis.py
@@ -2,3 +2,5 @@
 #Download our data from openneu.ro
 get_ipython().magic('history -g')
 get_ipython().magic('ls')
+Path('data_not_in_repo').mkdir()
+get_ipython().magic('mv metasearch/ data_not_in_repo/')
