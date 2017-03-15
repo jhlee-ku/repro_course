5c59c9efcbad5ec4c2ff86ddbb95b18a58884422
Jong-Hwan Lee <jhlee.jonghwanlee@gmail.com>
2017-03-15 10:23:09 -0400

    add metasearch_analysis.py

---
+++ metasearch_analysis.py
@@ -0,0 +1,5 @@
+# coding: utf-8
+#get_ipython().system('git clone https://github.com/OpenNeuroLab/metasearch.git')
+#get_ipython().magic('history -n -g')
+roi_volumes = [2.73, 145.3, 12.7, 16.2, 'a', 'b']
+get_ipython().magic('history -g')
